f575fa31264aa1e9de962df7d852a2b8871671e6
Elias Geiger <elias@ele-dev.de>
2024-11-29 22:58:39 +0100

    Added device info fetch function

--- data/script.js
+++ data/script.js
@@ -1,16 +1,39 @@
+// create global http instance for AJAX requests
+const xhttp = new XMLHttpRequest();
+
 // legacy function to handle checkbox change events 
 function toggleCheckbox(element) {
-    let xhr = new XMLHttpRequest();
     if (element.checked) { 
-        xhr.open("GET", "/gpio_update?output=" + element.id + "&state=1", true); 
+        xhttp.open("GET", "/gpio_update?output=" + element.id + "&state=1"); 
     } else { 
-        xhr.open("GET", "/gpio_update?output=" + element.id + "&state=0", true); 
+        xhttp.open("GET", "/gpio_update?output=" + element.id + "&state=0"); 
     }
-    xhr.send();
+    xhttp.send();
+}
+
+function fetchAndDisplayDeviceInfo() {
+    console.log("fetching device info stats ...");
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // display the receceived json encoded device info
+            let response = JSON.parse(this.responseText);
+            document.getElementById("device-name").innerHTML = response["device-name"];
+            document.getElementById("uptime").innerHTML = response["uptime"];
+            document.getElementById("wifi-ssid").innerHTML = response["wifi-ssid"];
+            document.getElementById("ipv4-address").innerHTML = response["ipv4-address"];
+            document.getElementById("mac-address").innerHTML = response["mac-address"];
+            document.getElementById("hostname").innerHTML = response["hostname"];
+            document.getElementById("cloud-connection-status").innerHTML = response["cloud-connection-status"];
+            // ...
+        }
+    }
+    xhttp.open("GET", "/device_info");
+    xhttp.send();
 }
 
 // method to update all displayed sensor measurements at once by passing a json object
 function setAllDisplayedMeasurements(measurements) {
+    console.log("updating measurment displays ...");
     document.getElementById("window-state").innerHTML = measurements['window-state'];
     document.getElementById("air-temperature").innerHTML = measurements['air-temperature'];
     document.getElementById("air-humidity").innerHTML = measurements['air-humidity'];
@@ -21,7 +44,6 @@ function setAllDisplayedMeasurements(measurements) {
 // setup periodic background polling of air temperature using AJAX
 let pollingPeriod = 4000;
 setInterval(function () {
-    let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
 
@@ -30,6 +52,6 @@ setInterval(function () {
             setAllDisplayedMeasurements(response);
         }
     };
-    xhttp.open("GET", "/sensor_read", true);
+    xhttp.open("GET", "/sensor_read");
     xhttp.send();
 }, pollingPeriod);
