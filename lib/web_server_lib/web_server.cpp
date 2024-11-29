def870f5e283e413fd590ee29d9dd07e2535db9d
Elias Geiger <elias@ele-dev.de>
2024-11-29 20:08:28 +0100

    removed legacy code from webserver

--- lib/web_server_lib/web_server.cpp
+++ lib/web_server_lib/web_server.cpp
@@ -10,102 +10,18 @@
 // define http server instance on default port 80
 AsyncWebServer server(80);
 
-// html template reused with placeholders
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML>
-<html>
-<head>
-  <title>ESP Web Server</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,">
-</head>
-<body>
-  <form action="/login">
-    username: <input type="text" name="username">
-    password: <input type="text" name="password">
-    <input type="submit" value="Submit">
-  </form><br>
-  %BUTTONPLACEHOLDER%
-  <p>
-    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature</span> 
-    <span id="temperature">%TEMPERATURE%</span>
-    <sup class="units">&deg;C</sup>
-  </p>
-  <p>
-    <i class="fas fa-tint" style="color:#00add6;"></i> 
-    <span class="dht-labels">Humidity</span>
-    <span id="humidity">%HUMIDITY%</span>
-    <sup class="units">&percnt;</sup>
-  </p>
-
-  <script>
-  function toggleCheckbox(element) {
-    var xhr = new XMLHttpRequest();
-    if(element.checked){ xhr.open("GET", "/gpio_update?output="+element.id+"&state=1", true); }
-    else { xhr.open("GET", "/gpio_update?output="+element.id+"&state=0", true); }
-    xhr.send();
-    }
-
-  setInterval(function ( ) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("temperature").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "/temperature", true);
-    xhttp.send();
-    }, 10000 ) ;
-
-  setInterval(function ( ) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("humidity").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "/humidity", true);
-    xhttp.send();
-    }, 10000 ) ;
-   </script>
-</body></html>)rawliteral";
-
 String outputState(int output)
 {
     return digitalRead(output) ? "checked" : "";
 }
 
-// Replaces placeholder
-String processor(const String &var)
-{
-    if (var == "BUTTONPLACEHOLDER")
-    {
-        String buttons = "";
-        buttons += "Output - GPIO " + String(HTTP_OUTPUT_PIN) + "<label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(HTTP_OUTPUT_PIN) + "\" " + outputState(HTTP_OUTPUT_PIN) + "><span class=\"slider\"></span></label>";
-        return buttons;
-    }
-    else if (var == "TEMPERATURE")
-    {
-        return String(dht_sensor_get_temperature());
-    }
-    else if (var == "HUMIDITY")
-    {
-        return String(dht_sensor_get_humidity());
-    }
-    return String();
-}
-
-void not_http_found(AsyncWebServerRequest *request)
+// callback for http requests on undefined routes
+void on_http_not_found(AsyncWebServerRequest *request)
 {
     request->send(404, "text/plain", "Not found");
 }
 
-void on_http_root(AsyncWebServerRequest *request)
-{
-    request->send_P(200, "text/html", index_html, processor);
-}
-
+// callback for login attempts
 void on_http_login(AsyncWebServerRequest *request)
 {
     String input_username = "No message sent";
@@ -129,7 +45,8 @@ void on_http_login(AsyncWebServerRequest *request)
     request->send(200, "text/html", "HTTP GET request with user login send to esp32 (username: " + input_username + " | password: " + input_password + ")<br><a href=\"/\">Return to Home Page</a>");
 }
 
-void on_http_gpio_update(AsyncWebServerRequest *request)
+// callback function to write the digital output pin state via GET request
+void on_http_gpio_write(AsyncWebServerRequest *request)
 {
     String output_message;
     String state_message;
@@ -146,14 +63,16 @@ void on_http_gpio_update(AsyncWebServerRequest *request)
         state_message = "No message sent";
     }
 
+    // print info to serial console
     Serial.print("GPIO: ");
     Serial.print(output_message);
     Serial.print(" -> ");
     Serial.println(state_message);
+
     request->send(200, "text/plain", "OK");
 }
 
-void on_http_temperature(AsyncWebServerRequest *request)
+void on_http_temperature_read(AsyncWebServerRequest *request)
 {
     Serial.println("Temperature was just requested from client");
     float temperature = dht_sensor_get_temperature();
@@ -161,7 +80,7 @@ void on_http_temperature(AsyncWebServerRequest *request)
     request->send_P(200, "text/plain", String(temperature).c_str());
 }
 
-void on_http_humidity(AsyncWebServerRequest *request)
+void on_http_humidity_read(AsyncWebServerRequest *request)
 {
     Serial.println("Humidity was just requested from client");
     float humidity = dht_sensor_get_humidity();
@@ -169,39 +88,28 @@ void on_http_humidity(AsyncWebServerRequest *request)
     request->send_P(200, "text/plain", String(humidity).c_str());
 }
 
-// List of GPIO pins to monitor
-const int gpioPins[] = { 18 }; 
-const int numPins = sizeof(gpioPins) / sizeof(gpioPins[0]);
-String html = "<html><body><h1>ESP32 GPIO States</h1><table border='1'><tr><th>Pin</th><th>State</th></tr>";
-
-void on_http_gpio_read_all(AsyncWebServerRequest *request) 
+void on_window_state_read(AsyncWebServerRequest *request)
 {
-    
-    for (int i = 0; i < numPins; i++) {
-      int state = digitalRead(gpioPins[i]);
-      html += "<tr><td>GPIO " + String(gpioPins[i]) + "</td><td>" + String(state) + "</td></tr>";
-    }
-    html += "</table></body></html>";
-    request->send(200, "text/html", html);
+    Serial.println("Window sensor state was just requested from client");
+    String state = digitalRead(18) == HIGH ? "open" : "closed";
+    request->send_P(200, "text/plain", state.c_str());
 }
 
+// setup function for the local async webserver
 int web_server_setup()
 {
     // serve static index.html stored in SPIFFS
     server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
 
     // define the async callback functions for different routes (= http paths)
-    // server.on("/", HTTP_GET, on_http_root);
     server.on("/login", HTTP_GET, on_http_login);
-    server.on("/gpio_update", HTTP_GET, on_http_gpio_update);
-    server.on("/temperature", HTTP_GET, on_http_temperature);
-    server.on("/humidity", HTTP_GET, on_http_humidity);
-    server.on("/gpio_read_all", HTTP_GET, on_http_gpio_read_all);
+    server.on("/gpio_write", HTTP_GET, on_http_gpio_write);
+    server.on("/temperature", HTTP_GET, on_http_temperature_read);
+    server.on("/humidity", HTTP_GET, on_http_humidity_read);
+    server.on("/window_state", HTTP_GET, on_window_state_read);
+    server.onNotFound(on_http_not_found);
 
-    server.onNotFound(not_http_found);
     server.begin();
 
-    Serial.print("Web server is setup and reachable via ipv4: ");
-    Serial.println(WiFi.localIP());
     return 0;
 }
