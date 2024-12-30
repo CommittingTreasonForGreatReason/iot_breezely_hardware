ccb035f10e97feaa6f1ffa36d94a5fcc0224272f
Elias Geiger <elias@ele-dev.de>
2024-12-30 17:04:49 +0100

    moved wifi debug method out of main.cpp

--- lib/serial_print_logger/logger.hpp
+++ lib/serial_print_logger/logger.hpp
@@ -1,3 +1,4 @@
+#pragma once
 
 typedef enum serial_log_level
 {
--- src/main.cpp
+++ src/main.cpp
@@ -15,36 +15,6 @@
 #include "logger.hpp"
 #include "esp_timer.h"
 
-void log_wifi_info(serial_log_level_t log_level)
-{
-    char buffer[128] = {0};
-    serial_logger_print("\n######### WIFI INFO #########", log_level);
-
-    sprintf(buffer, "SSID %s", WiFi.SSID().c_str());
-    serial_logger_print(buffer, log_level);
-
-    sprintf(buffer, "Gateway IPv4: %s", WiFi.gatewayIP().toString().c_str());
-    serial_logger_print(buffer, log_level);
-    serial_logger_print("------------------------------", log_level);
-
-    sprintf(buffer, "mDNS Hostname: %s.local", HOSTNAME);
-    serial_logger_print(buffer, log_level);
-
-    sprintf(buffer, "Hostname: %s", WiFi.getHostname());
-    serial_logger_print(buffer, log_level);
-
-    sprintf(buffer, "Local IPv4: %s", WiFi.localIP().toString().c_str());
-    serial_logger_print(buffer, log_level);
-
-    sprintf(buffer, "Local IPv6: %s", WiFi.localIPv6().toString().c_str());
-    serial_logger_print(buffer, log_level);
-
-    sprintf(buffer, "MAC address: %s", WiFi.macAddress().c_str());
-    serial_logger_print(buffer, log_level);
-
-    serial_logger_print("#############################", log_level);
-}
-
 int start_mDNS_timeout_us(int64_t timeout_us)
 {
     int64_t timeout_deadline_us = esp_timer_get_time() + timeout_us;
